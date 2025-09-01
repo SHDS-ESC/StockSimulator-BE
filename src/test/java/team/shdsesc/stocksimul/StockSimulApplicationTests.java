5173bcac5255cb729144dd8512e0e00b3dd4d775
SYL <yunisy51@gmail.com>
2025-09-01 12:50:00 +0900

    Init Settring

---
+++ src/test/java/team/shdsesc/stocksimul/StockSimulApplicationTests.java
@@ -0,0 +1,13 @@
+package team.shdsesc.stocksimul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
