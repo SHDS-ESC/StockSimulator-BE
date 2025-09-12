6f2ed524f5e397d7a572278e99d432ac654b29c3
SYL <yunisy51@gmail.com>
2025-09-12 10:38:23 +0900

    fix : Controller 메서드 리턴 타입 ResponseEntity로 수정

--- src/main/java/team/shdsesc/stocksimul/stock/StockController.java
+++ src/main/java/team/shdsesc/stocksimul/stock/StockController.java
@@ -2,6 +2,7 @@ package team.shdsesc.stocksimul.stock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,22 +20,24 @@ public class StockController {
     private final StockService stockService;
 
     @GetMapping("/list")
-    public List<StockEntity> getList() {
+    public ResponseEntity<List<StockEntity>> getList() {
         log.info("getList....");
         List<StockEntity> list = stockService.findAll();
-        return list;
+        return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{ticker}")
-    public StockEntity getStockByTicker(@PathVariable String ticker) {
+    public ResponseEntity<StockEntity> getStockByTicker(@PathVariable String ticker) {
         log.info("getStockByTicker...");
-        return stockService.findByTicker(ticker);
+        StockEntity stock = stockService.findByTicker(ticker);
+        return ResponseEntity.ok(stock);
     }
 
     @GetMapping("/real-time/{ticker}")
-    public List<RealTimeStockDTO> getRealTime(@PathVariable String ticker) {
+    public ResponseEntity<List<RealTimeStockDTO>> getRealTime(@PathVariable String ticker) {
         log.info("getRealTime... : " +  ticker);
-        return stockService.getRealTime(ticker);
+        List<RealTimeStockDTO> realTimeStockDTOList = stockService.getRealTime(ticker);
+        return ResponseEntity.ok(realTimeStockDTOList);
     }
 
 
--- src/main/java/team/shdsesc/stocksimul/stock/report/ReportController.java
+++ src/main/java/team/shdsesc/stocksimul/stock/report/ReportController.java
@@ -2,6 +2,7 @@ package team.shdsesc.stocksimul.stock.report;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping("{ticker}")
-    public List<ReportEntity> getReportByTicker(@PathVariable String ticker) {
+    public ResponseEntity<List<ReportEntity>> getReportByTicker(@PathVariable String ticker) {
         log.info("getReport...");
-        return reportService.findByTicker(ticker);
+        return ResponseEntity.ok(reportService.findByTicker(ticker));
     }
 
 }
