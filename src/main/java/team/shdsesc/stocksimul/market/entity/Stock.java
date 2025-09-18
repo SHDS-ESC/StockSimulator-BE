0b33d58e7acc8d6022932b7d496d0484623c12f4
jiwon <pjw99032986@gmail.com>
2025-09-18 12:18:56 +0900

    Feat: 보유 주식 불러오는 로직 추가

--- src/main/java/team/shdsesc/stocksimul/market/entity/Stock.java
+++ src/main/java/team/shdsesc/stocksimul/market/entity/Stock.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -15,6 +12,7 @@ import java.time.LocalDate;
 @Table(name = "stock")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Stock {
